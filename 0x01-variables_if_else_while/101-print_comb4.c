0532c314fcf93e40f138a90839e60ce6da201c73
ozzeal <ozygybaby1212@gmail.com>
2023-06-18 09:55:52 -0700

    C task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
 	{
 		for (y = x + 1; y < 9; y++)
 		{
-			for  (z = y + 1; z < 10;
+			for  (z = y + 1; z < 10; z++)
 			{
 				putchar((x % 10) + '0');
 				putchar((y % 10) + '0');
